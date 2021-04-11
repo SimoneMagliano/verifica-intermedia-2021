f85ab6b2631ae635c5581df26062d1b79a20f759
SimoneMagliano <77775924+SimoneMagliano@users.noreply.github.com>
2021-04-11 14:17:43 +0200

    Add files via upload

--- lib/UsersSearchFunctions.php
+++ lib/UsersSearchFunctions.php
@@ -25,5 +25,35 @@ function searchStatus(string $status) : callable {
         }
     };
 } 
+function sanitizeName($name) {
+
+    
+    $endSpace = preg_replace('/[^a-zA-Z]+$/','',$name);
+    $tinyName = preg_replace('/[^a-zA-Z ]+/','',$endSpace);
+    $onespaceName = preg_replace('/[ ]+/',' ',$tinyName);
+    $notags = trim($onespaceName, "<h1>");
+
+
+    $explodeName = explode(" ",$notags);
+    $correctNames = array_map('correctCase', $explodeName);
+    
+    
+    
+
+    return implode(" ", $correctNames);
+    
+
+   
+}
+
+
+function correctCase($name){
+    $nameLowercase = strtolower($name);
+    $uppercaseName = ucfirst($nameLowercase);
+
+
+    return $uppercaseName;
+
+}
 
 ?>
